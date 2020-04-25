2468144387dc5a58db5ad599c24851dedc48c016
Paulo Henrique <phenriq694@gmail.com>
2020-04-25 01:56:07 -0300

    FUNDAMENTALS: Javascript code organization.

---
+++ fundamentals/organization.js
@@ -0,0 +1,11 @@
+// Javascript is organized in sentences. The use of ';' at the end of the sentences is optional.
+console.log("Sentence 1")
+
+// It is also organized with block of codes
+{
+  // A block inside another block
+  {
+    console.log("Sentence 2")
+    console.log("Sentence 3")
+  }
+}
