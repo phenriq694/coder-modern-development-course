9527f04b8cb76398ad3812dfc0e01af31525f06d
Paulo Henrique <phenriq694@gmail.com>
2020-04-26 11:47:06 -0300

    FUNDAMENTALS: Template String

---
+++ fundamentals/templateString.js
@@ -0,0 +1,11 @@
+const name = 'Rebeca'
+const concatenation = 'Hello ' + name + '!'
+const template = `Hello ${name}!`
+
+console.log(concatenation, template)
+
+// Expressions...
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = text => text.toUpperCase()
+console.log(`Hey... ${up('wacth out')}!`)
