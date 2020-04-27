a3d9e0a3c38ed0ba35bf67e0f4962b79511473d4
Paulo Henrique <phenriq694@gmail.com>
2020-04-27 20:53:09 -0300

    FUNDAMENTALS: Declaring vars with 'var'

---
+++ fundamentals/var.js
@@ -0,0 +1,33 @@
+// 'var' have only two types of scope: Global or fucntion
+
+{
+  {
+    {
+      var canBeAccessed = 'yes'
+      console.log(canBeAccessed)
+    }
+  }
+}
+
+console.log(canBeAccessed)
+
+function functionScope() {
+  var local = 123
+  console.log(local)
+}
+
+functionScope()
+
+// console.log(local) Cannot be accessed outside the function.
+
+// 'var' declared in another scope, but, with the same name, it can be overwritten
+var num = 1
+
+{
+  var num = 2
+  // Output: 2
+  console.log(`Inside = ${num}`)
+}
+
+// Output: 2
+console.log(`Outside = ${num}`)
