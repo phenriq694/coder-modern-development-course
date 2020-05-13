72a311dffb40587540ee5c9fa084d2a719751801
Paulo Henrique <phenriq694@gmail.com>
2020-05-12 22:27:58 -0300

    FUNDAMENTALS: Unary Operators

---
+++ fundamentals/unaryOperators.js
@@ -0,0 +1,15 @@
+let num1 = 1
+let num2 = 2
+
+num1++ 
+// Output: 2
+console.log(num1)
+--num1
+// Output: 1
+console.log(num1)
+
+// Output: true
+console.log(++num1 === num2--)
+
+// Output: false
+console.log(num1 === num2)
